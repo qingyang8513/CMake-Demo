011b7f7cd6e906303c9e92b45d4c4029a4e83823
qingyang8513 <44761102+qingyang8513@users.noreply.github.com>
2019-03-19 18:35:16 +0800

    Add files via upload

---
+++ Demo3/include/mymath.h
@@ -0,0 +1,17 @@
+#ifndef _MYMATH_H_
+#define _MYMATH_h_
+
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int add(int a, int b);
+int sub(int a, int b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Demo4/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "math/include/mymath.h"
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, Demo4.\n");
+
+    printf("1 + 5 = %d\n", add(1, 5));
+    
+    return 0;
+}
